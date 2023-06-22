909c2889ac7bc69ab2989b1b6ecb4c3259088155
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2023-06-22 22:45:13 +0700

    edit header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,24 +1,202 @@
-<script> 
-import { page } from '$app/stores';
-import logo from '$lib/images/logo.png';
-import github from '$lib/images/github.svg';
-import classnames from 'classnames';
-const cx = classnames.bind();
+<script>
+  import { page } from '$app/stores';
+  import logo from '$lib/images/logo.png';
+  import github from '$lib/images/github.svg';
 
-let isMenuCollapsed = true;
-let navbar;
-
-function toggleMenu() {
-  isMenuCollapsed = !isMenuCollapsed;
-  if (isMenuCollapsed) {
-    navbar.classList.add('collapsed');
-  } else {
-    navbar.classList.remove('collapsed');
-  }
-}
+  // based on suggestions from:
+  // Inclusive Components by Heydon Pickering https://inclusive-components.design/collapsible-sections/
+  export let navMenu;
+  let expanded =false;
 </script>
- <header id="top"> <div class="site-logo"> <div class="corner"> <a href="/"> <img src={logo} alt="Coding Nguyễn" /> </a> </div> <ul class="nav-item"> <li class="nav-item"> <a class="skip" href="#nav">Skip to main navigation</a> </li> <li class="nav-item"> <a class="skip" href="#main">Skip to main content</a> </li> <li class="nav-item"> <a class="skip" href="#footer">Skip to footer</a> </li> </ul> </div> <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top" id="nav"> <svg viewBox="0 0 2 3" aria-hidden="true"> <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" /> </svg> <button class="navbar-toggler" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" on:click={toggleMenu}>
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
- <ul class="navbar-nav ms-auto"> <li class="nav-menu"> <a class={cx({ 'nav-link': true, active: $page.url.pathname === '/' })} href="/" aria-current={$page.url.pathname === '/' ? 'page' : undefined} > home </a> </li> <li class="nav-menu"> <a class={cx({ 'nav-link': true, active: $page.url.pathname === '/about' })} href="/about" aria-current={$page.url.pathname === '/about' ? 'page' : undefined} > about </a> </li> <li class="nav-menu"> <a class={cx({ 'nav-link': true, active: $page.url.pathname.startsWith('/blog') })} href="/blog" aria-current={($page.url.pathname.startsWith('/blog')) ? 'page' : undefined} > blog </a> </li> <li class="nav-menu"> <a class={cx({ 'nav-link': true, active: $page.url.pathname === '/contact' })} href="/contact" aria-current={$page.url.pathname === '/contact' ? 'page' : undefined} > contact </a> </li> </ul> </div> <div class="corner"> <a href="https://github.com/sveltejs/kit"> <img src={github} alt="GitHub" /> </a> </div> </nav> </header> <style> :root { --color-theme-1: #007bff; --color-background: rgba(255, 255, 255, 0.7); --color-text: #fff; } header { display: flex; justify-content: space-between; } .corner { width: 3em; height: 3em; display: flex; align-items: center; justify-content: center; } .corner img { width: 2em; height: 2em; object-fit: contain; } .site-logo { display: flex; align-items: center; justify-content: space-between; } .nav-item { display: flex; flex-wrap: wrap; align-items: center; margin-left: 1rem; } .nav-item .nav-item { margin-left: 2rem; } .nav-link { display: block; padding: 0.5rem; color: var(--color-text); font-weight: 700; font-size: 0.8rem; text-transform: uppercase; letter-spacing: 0.1em; text-decoration: none; transition: color 0.2s linear; } .nav-link.active { color: var(--color-theme-1); border-bottom: 2px solid var(--color-theme-1); } .nav-link:hover { color: var(--color-theme-1); } nav { display: flex; justify-content: center; background: var(--color-background); } svg { width: 2em; height: 3em; display: block; } path { fill: var(--color-background); } ul { padding: 0; margin: 0; display: flex; justify-content: center; align-items: center; list-style: none; flex-wrap: wrap; height: 3em; } li { position: relative; height: 100%; white-space: nowrap; } li[aria-current='page']:: performance { --size: 6px; content: ''; width: 0; height: 0; position: absolute; top: 0; left: calc(50% - var(--size)); border: var(--size) solid transparent; border-top: var(--size) solid var(--color-theme-1); } .collapse { height: 0; transition: height 0.35s ease; overflow: hidden; } .collapse:not(.collapsed) { height: auto; } .collapsed { height: 0; } @media (max-width: 991px) { .nav-item { position: absolute; top: 4rem; left: 0; z-index: 10; width: 100%; box-shadow: 0 2px 6px 0 rgba(0,0,0,.2); background: var(--color-background); } .nav-item.collapsed { display: none; } .navbar-toggler { display: flex; } .nav-menu { display: block; text-align: center; margin-top: 1rem; } } </style>
+
+<header id="top">
+  <div class="site-logo">
+    <div class="corner">
+      <a href="/">
+        <img src={logo} alt="Coding Nguyễn" />
+      </a>
+    </div>
+    <ul class="nav-item">
+      <li class="nav-item">
+        <a class="skip" href="#nav">Skip to main navigation</a>
+      </li>
+      <li class="nav-item">
+        <a class="skip" href="#main">Skip to main content</a>
+      </li>
+      <li class="nav-item">
+        <a class="skip" href="#footer">Skip to footer</a>
+      </li>
+    </ul>
+  </div>
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top" id="nav">
+    <svg viewBox="0 0 2 3" aria-hidden="true">
+      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+    </svg>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded={expanded}
+      aria-label="Toggle navigation"
+      on:click={() => (expanded = !expanded)}
+    >
+      {navMenu}
+      <span class="navbar-toggler-icon"></span>
+      <svg viewBox="0 0 20 20" fill="none">
+        <path class="vert" d="M10 1V19" stroke="black" stroke-width="2" />
+        <path d="M1 10L19 10" stroke="black" stroke-width="2" />
+      </svg>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive" hidden={!expanded}>
+        <ul id="main-menu" class="navbar-nav ml-auto">
+          <li
+            aria-current={
+              $page.url.pathname === '/' ? 'page' : undefined
+            }
+          >
+            <a href="/">Home</a>
+          </li>
+          <li
+            aria-current={
+              $page.url.pathname === '/about' ? 'page' : undefined
+            }
+          >
+            <a href="/about">About</a>
+          </li>
+          <li
+            aria-current={
+              $page.url.pathname.startsWith('/blog') ? 'page' : undefined
+            }
+          >
+            <a href="/blog">blog</a>
+          </li>
+          <li
+            aria-current={
+              $page.url.pathname === '/contact' ? 'page' : undefined
+            }
+          >
+            <a href="/contact">contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="corner">
+        <a href="https://github.com/sveltejs/kit">
+          <img src={github} alt="GitHub" />
+        </a>
+      </div>
+  </nav>
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .site-logo {
+    display: flex;
+    align-items: center;
+  }
+
+  .corner {
+    width: 3em;
+    height: 3em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .corner img {
+    width: 2em;
+    height: 2em;
+    object-fit: contain;
+  }
+
+  .nav-item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 1rem;
+  }
+
+  .nav-item li {
+    list-style: none;
+  }
+
+  .nav-item a {
+    color: #1d1d1f;
+    text-decoration: none;
+    font-size: 0.8rem;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
+
+  .navbar {
+    background-color: #fff;
+    color: #1d1d1f;
+    font-weight: bold;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+  }
+
+  #nav {
+    display: flex;
+    align-items: center;
+    box-shadow: none;
+  }
+
+  .navbar-toggler {
+    color: #1d1d1f;
+    border: none;
+    background: transparent;
+    padding: 0;
+    outline: none;
+  }
+
+  .navbar-toggler svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  .navbar-toggler span {
+    display: none;
+  }
+
+  .navbar-toggler[aria-expanded="true"] svg.vert {
+    display: none;
+  }
+
+  .navbar-toggler[aria-expanded='true'] svg.horz {
+    display: block;
+  }
+
+  .vert {
+    width: 2em;
+    height: 3em;
+    display: block;
+  }
+
+  path {
+    fill: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding-left: 0;
+    display: flex;
+    list-style: none;
+  }
+
+  li[aria-current='page']::before {
+    content: '';
+    height: 4px;
+    width: 100%;
+    background-color: #1d1d1f;
+    position: absolute;
+    bottom: -10px;
+    left: 0;
+  }
+</style>
