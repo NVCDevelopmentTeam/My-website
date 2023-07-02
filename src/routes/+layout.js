eab1dcfbe35df6bdfc4e89e851f69f9ed02d31da
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-02 22:51:39 +0700

    add code

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,18 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+import { error } from '@sveltejs/kit'
+
+// Ensures all pages under this layout (which is all of them) are statically prerendered at build time 
+export const prerender = true
+
+// Allows client side routing. Necessary for page transitions and link prefetching; change to false if you prefer ordinary routing without JS
+export const csr = true
+
+export const load = async ({ url, fetch }) => {
+	try {
+		return {
+			path: url.pathname
+		}
+	}
+	catch(err) {
+		throw error(500, err)
+	}
+}
