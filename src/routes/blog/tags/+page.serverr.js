0182b3b1b6687b1774c160d163ee9729306f3bdf
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-05 21:17:43 +0700

    Update

---
+++ src/routes/blog/tags/+page.serverr.js
@@ -0,0 +1,24 @@
+import fetchPosts from '$lib/data/fetchPosts'
+
+export async function get() {
+  // Perform some server-side logic here
+  const data = await fetchDataFromDatabase();
+
+  // Return the data as a response
+  return {
+    body: { data },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+async function fetchDataFromDatabase() {
+  // Simulate fetching data from a database
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const data = ['apple', 'banana', 'cherry'];
+      resolve(data);
+    }, 1000);
+  });
+}
