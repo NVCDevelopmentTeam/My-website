e7cecfa2ee1952e5d932b5c9d6619a0ecaef415e
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-16 12:25:46 +0700

    Add file

---
+++ src/lib/components/Latest-posts.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let posts = [];
+
+  onMount(async () => {
+    const response = await fetch('/api/latest-posts');
+    posts = await response.json();
+  });
+</script>
+
+<div class="latest-posts">
+  <h2>Latest posts</h2>
+
+  {#each posts as post}
+    <div class="latest-post">
+      <p class="latest-post-title">{post.title}</p>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .latest-posts {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .latest-post {
+    margin-bottom: 10px;
+  }
+
+  .latest-post-title {
+    font-size: 16px;
+    font-weight: bold;
+  }
+</style>
