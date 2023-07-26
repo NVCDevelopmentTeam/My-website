59b8ae5eaacef1254e87a4c1ab2a701bad67e1f1
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-26 22:40:47 +0700

    update code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -1,43 +1,16 @@
-import { postsPerPage } from '$lib/data/config'
-
-const fetchPosts = async ({ offset = 0, limit = postsPerPage, category = '' } = {}) => {
-
-	const posts = await Promise.all(
-		Object.entries(import.meta.glob('/src/lib/posts/*.md')).map(async ([path, resolver]) => {
-			const { metadata } = await resolver()
-			const slug = path.split('/').pop().slice(0, -3)
-			return { ...metadata, slug }
-		})
-	)
-
-	let sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date))
-	
-	if (category) {
-    sortedPosts = sortedPosts.filter(post => post.categories.includes(category))
-	}
-  
-	if (offset) {
-		sortedPosts = sortedPosts.slice(offset)
-	}
-	
-	if (limit && limit < sortedPosts.length && limit != -1) {
-		sortedPosts = sortedPosts.slice(0, limit)
-	}
-
-	sortedPosts = sortedPosts.map(post => ({
-		title: post.title,
-		slug: post.slug,
-		excerpt: post.excerpt,
-		coverImage: post.coverImage,
-		coverWidth: post.coverWidth, 
-		coverHeight: post.coverHeight,
-		date: post.date,
-		categories: post.categories,
-	}))
-
-	return {
-		posts: sortedPosts
-	}
-}
-
-export default fetchPosts
+// in fetchPosts.js
+  const posts = await Promise.all(
+    // added "as: 'raw'" as an option
+    Object.entries(import.meta.glob('/src/lib/posts/*.md', { as: 'raw' })).map(
+      async ([path, resolver]) => {
+        console.log(path);
+        // get the raw content of the file
+        const raw = await resolver();
+        // convert the markdown and extract metadata
+        const { metadata } = parseMD(raw);  
+        console.log(metadata);
+        const slug = path.split('/').pop().slice(0, -3);
+        return { ...metadata, slug };
+      }
+    )
+  );
