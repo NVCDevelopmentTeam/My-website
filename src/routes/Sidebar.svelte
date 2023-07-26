6835b158d92507cbc091b6ff939890f13ed59996
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-26 12:08:22 +0700

    update code

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -1,16 +1,34 @@
 <script>
-import LatestPosts from '$lib/components/LatestPosts.svelte';
-import SocialLinks from '$lib/components/SocialLinks.svelte';
+  import LatestPosts from '$lib/components/LatestPosts.svelte';
+  import SocialLinks from '$lib/components/SocialLinks.svelte';
 </script>
 
-<Sidebar>
-  <LatestPosts />
-  <SocialLinks />
-</Sidebar>
+<sidebar class="sidebar">
+  <div class="latest-posts">
+    <LatestPosts />
+  </div>
+  <div class="social-links">
+    <SocialLinks />
+  </div>
+</sidebar>
 
 <style>
-.sidebar {
-  background-color: #f5f5f5;
-  padding: 1rem;
-}
+  /* Add your additional CSS styles here */
+  .sidebar {
+    padding: 1rem;
+    background-color: #f5f5f5;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  /* Add any specific styles for the LatestPosts and SocialLinks components */
+  .latest-posts {
+    margin-bottom: 1rem;
+  }
+
+  .social-links {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
