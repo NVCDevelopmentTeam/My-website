641a76257c8ad18d14f875005ebaea4a2c862d14
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-27 12:29:57 +0700

    edit code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -1,17 +1,19 @@
 import { postsPerPage } from '/config';
-// in fetchPosts.js
+import { parseMD } from './parseMD';
+
+export async function fetchPosts() {
   const posts = await Promise.all(
-    // added "as: 'raw'" as an option
-    Object.entries(import.meta.glob('/src/lib/posts/*.md', { as: 'raw' })).map(
+    Object.entries(import.meta.glob('/src/lib/posts/*.md')).map(
       async ([path, resolver]) => {
         console.log(path);
-        // get the raw content of the file
-        const raw = await resolver();
-        // convert the markdown and extract metadata
-        const { metadata } = parseMD(raw);  
+        const { default: raw } = await resolver();
+        const { metadata } = parseMD(raw);
         console.log(metadata);
         const slug = path.split('/').pop().slice(0, -3);
         return { ...metadata, slug };
       }
     )
   );
+
+  return posts;
+}
