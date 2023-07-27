fa65b691573e65d86a02249504b1aec521438367
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-27 19:14:57 +0700

    update code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -2,11 +2,11 @@ import { postsPerPage } from '/config';
 import { parseMD } from './parseMD';
 
 export async function fetchPosts() {
-  const postPaths = await import.meta.globEager('/src/lib/posts/*.md');
+  const postPaths = Object.keys(import.meta.globEager('/src/lib/posts/*.md'));
 
   const posts = await Promise.all(
     postPaths.map(async (path) => {
-      const raw = await import.meta.globEager(path);
+      const raw = import.meta.globEager(path).default;
       const metadata = parseMD(raw);
       const slug = path.split('/').pop().slice(0, -3);
       return { ...metadata, slug };
