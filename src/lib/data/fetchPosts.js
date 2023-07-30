b097307a20944158fde2483087ebc9ccae9f10ae
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-30 21:18:05 +0700

    update code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -1,13 +1,12 @@
 import { postsPerPage } from '/config';
 import { parseMD } from '$lib/posts/parseMD';
-import md from 'rollup-plugin-md';
 
 export async function fetchPosts() {
   const postPaths = Object.keys(import.meta.globEager('$lib/posts/*.md'));
   const posts = await Promise.all(
     postPaths.map(async (path) => {
-      const raw = await import(`${path}`);
-      const metadata = parseMD(raw); // Use raw since it's an ES6 import
+      const { default: raw } = await import(`${path}?raw`); // Add '?raw' to import the raw file content
+      const metadata = parseMD(raw);
       const slug = path.split('/').pop().slice(0, -3);
       return {
         ...metadata,
