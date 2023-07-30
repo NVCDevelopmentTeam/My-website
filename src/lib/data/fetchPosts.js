c86e40649c226ec3b9b529712a2b4f45d56cf521
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-30 21:46:37 +0700

    update code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -1,19 +1,43 @@
-import { postsPerPage } from '$lib/data/config';
-import { parseMD } from '$lib/posts/parseMD';
+import { postsPerPage } from '$lib/data/config'
 
-export async function fetchPosts() {
-  const postPaths = Object.keys(import.meta.globEager('$lib/posts/*.md'));
-  const posts = await Promise.all(
-    postPaths.map(async (path) => {
-      const { default: raw } = await import(`${path}?raw`); // Add '?raw' to import the raw file content
-      const metadata = parseMD(raw);
-      const slug = path.split('/').pop().slice(0, -3);
-      return {
-        ...metadata,
-        slug,
-      };
-    })
-  );
+const fetchPosts = async ({ offset = 0, limit = postsPerPage, category = '' } = {}) => {
 
-  return posts.slice(0, postsPerPage);
+	const posts = await Promise.all(
+		Object.entries(import.meta.glob('/src/lib/posts/*.md')).map(async ([path, resolver]) => {
+			const { metadata } = await resolver()
+			const slug = path.split('/').pop().slice(0, -3)
+			return { ...metadata, slug }
+		})
+	)
+
+	let sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date))
+	
+	if (category) {
+    sortedPosts = sortedPosts.filter(post => post.categories.includes(category))
+	}
+  
+	if (offset) {
+		sortedPosts = sortedPosts.slice(offset)
+	}
+	
+	if (limit && limit < sortedPosts.length && limit != -1) {
+		sortedPosts = sortedPosts.slice(0, limit)
+	}
+
+	sortedPosts = sortedPosts.map(post => ({
+		title: post.title,
+		slug: post.slug,
+		excerpt: post.excerpt,
+		coverImage: post.coverImage,
+		coverWidth: post.coverWidth, 
+		coverHeight: post.coverHeight,
+		date: post.date,
+		categories: post.categories,
+	}))
+
+	return {
+		posts: sortedPosts
+	}
 }
+
+export default fetchPosts
