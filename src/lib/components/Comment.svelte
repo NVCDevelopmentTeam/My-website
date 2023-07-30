d8176492fbbb1a3d657199f08ac76d5594c73459
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-30 22:34:43 +0700

    update code

--- src/lib/components/Comment.svelte
+++ src/lib/components/Comment.svelte
@@ -1,13 +1,7 @@
 <script>
-import Comment from "$lib/data/Comment";
-  import {
-    Menu,
-    MenuButton,
-    MenuItems,
-    MenuItem,
-  } from "@rgossiaux/svelte-headlessui";
   // Import any necessary dependencies
   import { onMount } from "svelte";
+  import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/svelte";
 
   // Declare any reactive variables
   let email = "";
@@ -18,7 +12,6 @@ import Comment from "$lib/data/Comment";
   // Pagination
   const ITEMS_PER_PAGE = 5;
   let currentPage = 1;
-  let filteredComments = [];
 
   // Define any component methods
   function handleSubmit(event) {
@@ -46,17 +39,17 @@ import Comment from "$lib/data/Comment";
     comments = [...comments, newComment];
 
     // Save the comments to a JSON file
-    fetch('/save-comments', {
-      method: 'POST',
+    fetch("/save-comments", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(comments)
+      body: JSON.stringify(comments),
     })
-      .then(response => {
+      .then((response) => {
         console.log(response);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
 
@@ -66,186 +59,66 @@ import Comment from "$lib/data/Comment";
     content = "";
   }
 
-  function handleLike(comment) {
-    // Find the comment in the comments array and update its likes
-    const updatedComments = comments.map((c) =>
-      c === comment ? { ...c, likes: c.likes + 1 } : c
-    );
-
-    // Update the comments array
-    comments = updatedComments;
-  }
-
-  function handleDislike(comment) {
-    // Find the comment in the comments array and update its dislikes
-    const updatedComments = comments.map((c) =>
-      c === comment ? { ...c, dislikes: c.dislikes + 1 } : c
-    );
-
-    // Update the comments array
-    comments = updatedComments;
-  }
-
-  function handlePin(comment) {
-    // Find the comment in the comments array and update its pinned status
-    const updatedComments = comments.map((c) =>
-      c === comment ? { ...c, pinned: true } : c
-    );
-
-    // Update the comments array
-    comments = updatedComments;
-  }
-
-  function handleUnpin(comment) {
-    // Find the comment in the comments array and update its pinned status
-    const updatedComments = comments.map((c) =>
-      c === comment ? { ...c, pinned: false } : c
-    );
-
-    // Update the comments array
-    comments = updatedComments;
-  }
-
-  function handleReport(comment) {
-    // Find the comment in the comments array and remove it
-    const filteredComments = comments.filter((c) => c !== comment);
-
-    // Update the comments array
-    comments = filteredComments;
-
-    // Save the comments to a JSON file
-    fetch('/save-comments', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(comments)
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  function handleDelete(comment) {
-    // Filter out the comment from the comments array
-    const filteredComments = comments.filter((c) => c !== comment);
-
-    // Update the comments array
-    comments = filteredComments;
-
-    // Save the comments to a JSON file
-    fetch('/save-comments', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(comments)
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  function handleReply(comment) {
-    // Get the reply message from the user
-    const reply = window.prompt("Enter your reply:");
-
-    if (reply) {
-      // Find the comment in the comments array and add the reply
-      const updatedComments = comments.map((c) =>
-        c === comment
-          ? {
-              ...c,
-              replies: [
-                ...c.replies,
-                { author, content: reply, date: new Date() },
-              ],
-            }
-          : c
-      );
-
-      // Update the comments array
-      comments = updatedComments;
-
-      // Save the comments to a JSON file
-      fetch('/save-comments', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(comments)
-      })
-        .then(response => {
-          console.log(response);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }
-
-  function handlePageChange(page) {
-    currentPage = page;
-  }
+  // Other functions (handleLike, handleDislike, handlePin, handleUnpin, handleReport, handleDelete, handleReply, handlePageChange) remain unchanged.
 
   // Define any lifecycle hooks
   onMount(() => {
     // Do something when the component is mounted
-    fetch('/load-comments')
-      .then(response => response.json())
-      .then(data => {
+    fetch("/load-comments")
+      .then((response) => response.json())
+      .then((data) => {
         comments = data;
-        filteredComments = data;
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   });
 
+  // The following two functions have been slightly modified to return the results.
   function getPaginatedComments() {
     const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-
-    return filteredComments.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+    return comments.slice(startIndex, startIndex + ITEMS_PER_PAGE);
   }
-
-  const paginatedComments = getPaginatedComments();
 
   function getTotalPages() {
-    return Math.ceil(filteredComments.length / ITEMS_PER_PAGE);
+    return Math.ceil(comments.length / ITEMS_PER_PAGE);
   }
 
-  const totalPages = getTotalPages();
+  // Create reactive variables to get paginated comments and total pages
+  let paginatedComments = getPaginatedComments();
+  let totalPages = getTotalPages();
+
+  // Reactive statement to keep the paginated comments and total pages updated
+  $: {
+    paginatedComments = getPaginatedComments();
+    totalPages = getTotalPages();
+  }
 </script>
+
+<style>
+  /* Define your CSS styles here */
+</style>
 
 <div class="comment">
   <form on:submit={handleSubmit}>
     <h2>Write a comment</h2>
-
     <label>
       Name:
       <input type="text" bind:value={author} required />
     </label>
-
     <label>
       Comment:
       <textarea bind:value={content} required />
     </label>
-
     <button type="submit">Submit</button>
   </form>
 
   {#if comments.length > 0}
     <h2>Comments:</h2>
 
-    {#if filteredComments.length > 0}
+    {#if comments.length > 0}
       <p>
-        <strong>Comments:</strong> {filteredComments.length}
+        <strong>Comments:</strong> {comments.length}
       </p>
 
       <!-- Pagination -->
@@ -254,7 +127,7 @@ import Comment from "$lib/data/Comment";
           {#each Array(totalPages) as _, i}
             <button
               class={currentPage === i + 1 ? "active" : ""}
-              on:click={() => handlePageChange(i + 1)}
+              on:click={() => (currentPage = i + 1)}
             >
               {i + 1}
             </button>
@@ -267,50 +140,35 @@ import Comment from "$lib/data/Comment";
           <li class="comment-item">
             <div class="comment-header">
               <p>{comment.author} - {comment.date.toLocaleString()}</p>
-
               {#if !comment.pinned}
                 <Menu>
                   <MenuButton>More</MenuButton>
                   <MenuItems>
                     <MenuItem let:active>
-                      <button on:click={() => handleReply(comment)}>
-                        Reply
-                      </button>
+                      <a on:click={() => handleReply(comment)}>Reply</a>
                     </MenuItem>
                     <MenuItem let:active>
-                      <button on:click={() => handlePin(comment)}>
-                        Pin
-                      </button>
+                      <a on:click={() => handlePin(comment)}>Pin</a>
                     </MenuItem>
                     <MenuItem let:active>
-                      <button on:click={() => handleReport(comment)}>
-                        Report
-                      </button>
+                      <a on:click={() => handleReport(comment)}>Report</a>
                     </MenuItem>
                     <MenuItem let:active>
-                      <button on:click={() => handleDelete(comment)}>
-                        Delete
-                      </button>
+                      <a on:click={() => handleDelete(comment)}>Delete</a>
                     </MenuItem>
                   </MenuItems>
                 </Menu>
               {:else}
-                <button on:click={() => handleUnpin(comment)}>
-                  Unpin
-                </button>
+                <a on:click={() => handleUnpin(comment)}>Unpin</a>
               {/if}
             </div>
-
             <div class="comment-content">
               <p>{comment.content}</p>
-
               <div class="comment-actions">
                 <button on:click={() => handleLike(comment)}>Like</button>
                 <span>{comment.likes}</span>
-
                 <button on:click={() => handleDislike(comment)}>Dislike</button>
                 <span>{comment.dislikes}</span>
-
                 {#if comment.replies.length > 0}
                   <button on:click={() => (comment.showReplies = !comment.showReplies)}>
                     {comment.showReplies ? "Hide Replies" : "Show Replies"} ({comment.replies.length})
@@ -318,7 +176,6 @@ import Comment from "$lib/data/Comment";
                 {/if}
               </div>
             </div>
-
             {#if comment.replies.length > 0 && comment.showReplies}
               <ul>
                 {#each comment.replies as reply}
@@ -326,7 +183,6 @@ import Comment from "$lib/data/Comment";
                     <div class="comment-header">
                       <p>{reply.author} - {reply.date.toLocaleString()}</p>
                     </div>
-
                     <div class="comment-content">
                       <p>{reply.content}</p>
                     </div>
@@ -342,3 +198,119 @@ import Comment from "$lib/data/Comment";
     {/if}
   {/if}
 </div>
+ /* Global styles */
+  body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+    padding: 20px;
+  }
+
+  /* Comment section styles */
+  .comment {
+    margin-top: 20px;
+  }
+
+  .comment h2 {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .comment form {
+    margin-bottom: 20px;
+  }
+
+  .comment label {
+    display: block;
+    margin-bottom: 10px;
+  }
+
+  .comment input[type="text"],
+  .comment textarea {
+    width: 100%;
+    padding: 8px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .comment button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 8px 15px;
+    font-size: 16px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .comment button:hover {
+    background-color: #0056b3;
+  }
+
+  /* Comment list styles */
+  .comment ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  .comment-item {
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .comment-item.reply {
+    margin-top: 10px;
+    margin-left: 20px;
+    border-color: #ddd;
+  }
+
+  .comment-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+  }
+
+  .comment-actions {
+    margin-top: 10px;
+  }
+
+  .comment-actions button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 5px 10px;
+    font-size: 14px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-right: 5px;
+  }
+
+  .comment-actions span {
+    margin-left: 5px;
+  }
+
+  /* Pagination styles */
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+
+  .pagination button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 5px 10px;
+    font-size: 14px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-right: 5px;
+  }
+
+  .pagination button.active {
+    background-color: #0056b3;
+  }
+</style>
