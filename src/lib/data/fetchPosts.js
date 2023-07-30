926b2b31db44b0093d4aebe47ffc2b415182c2ae
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-30 08:06:31 +0700

    update code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -5,8 +5,8 @@ export async function fetchPosts() {
   const postPaths = Object.keys(import.meta.globEager('$lib/posts/*.md'));
   const posts = await Promise.all(
     postPaths.map(async (path) => {
-      const raw = await import(`$lib/posts/${path.slice(12)}`);
-      const metadata = parseMD(raw.default); // Use raw.default since it's an ESM import
+      const raw = await import(`${path}`);
+      const metadata = parseMD(raw); // Use raw since it's an ES6 import
       const slug = path.split('/').pop().slice(0, -3);
       return {
         ...metadata,
