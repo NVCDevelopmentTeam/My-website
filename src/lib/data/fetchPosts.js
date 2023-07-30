808c54e376ece7ecc18292b2934c2c09e3bc500d
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-30 07:27:13 +0700

    update code

--- src/lib/data/fetchPosts.js
+++ src/lib/data/fetchPosts.js
@@ -1,16 +1,17 @@
-// Assuming this code is in a file named `fetchPosts.js`
-
-import { postsPerPage } from '/config';
+import { postsPerPage } from '$lib/config';
 import { parseMD } from '$lib/posts/parseMD';
 
 export async function fetchPosts() {
   const postPaths = Object.keys(import.meta.globEager('$lib/posts/*.md'));
   const posts = await Promise.all(
     postPaths.map(async (path) => {
-      const raw = (await import(`/posts/${path.slice(12)}`)).default;
-      const metadata = parseMD(raw);
+      const raw = await import(`$lib/posts/${path.slice(12)}`);
+      const metadata = parseMD(raw.default); // Use raw.default since it's an ESM import
       const slug = path.split('/').pop().slice(0, -3);
-      return { ...metadata, slug };
+      return {
+        ...metadata,
+        slug,
+      };
     })
   );
 
