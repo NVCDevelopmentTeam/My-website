59f76f096cc7fd0a1faac79b8685559d5866e520
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-10-22 19:09:47 +0700

    update code

--- src/lib/data/search.js
+++ src/lib/data/search.js
@@ -1,19 +1,29 @@
 import { fetch, getResponse } from '@sveltejs/kit';
 
 export async function search(query) {
-  const response = await fetch(`https://example.com/api/search?q=${query}`);
+  // encode the query parameter to prevent errors
+  const encodedQuery = encodeURIComponent(query);
+  const response = await fetch(`https://example.com/api/search?q=${encodedQuery}`);
   return getResponse(response);
 }
 
 export async function get({ query }) {
-  const { q } = (query);
+  const { q } = query;
 
-  // call the `search` function with the query parameter
-  const searchQuery = JSON.stringify(q);
-  const results = await search(searchQuery);
-  
-  // return the results in the response body
-  return {
-    body: results
-  };
+  // check if the query parameter is valid
+  if (q && typeof q === 'string') {
+    // call the `search` function with the query parameter
+    const results = await search(q);
+
+    // return the results in the response body
+    return {
+      body: results
+    };
+  } else {
+    // return an error message if the query parameter is missing or invalid
+    return {
+      status: 400,
+      body: 'Invalid query parameter'
+    };
+  }
 }
