245512f3cdb6cbcfa2c1613a7164be0d1d5f7d21
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-10-22 18:59:44 +0700

    update code

--- src/lib/components/Comment.svelte
+++ src/lib/components/Comment.svelte
@@ -1,6 +1,7 @@
 <script>
   // Import any necessary dependencies
   import { onMount } from "svelte";
+import Comment from "$lib/data/comment';
   import { Divider, Text, Menu, MenuButton, MenuItems, MenuItem } from "svelte";
 
   // Declare any reactive variables
