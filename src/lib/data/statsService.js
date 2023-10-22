2dba846fd1a7b5e0c1f6aaea867f2a8691f0f5ea
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-10-22 07:46:14 +0700

    update code

--- src/lib/data/statsService.js
+++ src/lib/data/statsService.js
@@ -1,20 +1,21 @@
-import database from '$lib/data/database'
-import analyticsService from '$lib/data/analyticsService'
+import database from '$lib/data/database';
+import analyticsService from '$lib/data/analyticsService';
 
-async function getStats() {
-  const visitsToday = await analyticsService.getVisitsToday();
-  const totalVisits = await database.getTotalVisits();
-  const totalVisitors = await database.getTotalVisitors();
-  const totalCountries = await database.getTotalCountries();
+export async function getStats() {
+  try {
+    const visitsToday = await analyticsService.getVisitsToday();
+    const totalVisits = await database.getTotalVisits();
+    const totalVisitors = await database.getTotalVisitors();
+    const totalCountries = await database.getTotalCountries();
 
-  return {
-    visitsToday,
-    totalVisits,
-    totalVisitors,
-    totalCountries
-  };
+    return {
+      visitsToday,
+      totalVisits,
+      totalVisitors,
+      totalCountries
+    };
+  } catch (error) {
+    console.error('Error occurred while fetching stats:', error);
+    throw error; // Rethrow the error to be handled by the caller
+  }
 }
-
-export default {
-  getStats
-};
