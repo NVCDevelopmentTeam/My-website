2b88fe0c785ee025629c5088a2831fbd0e98a71e
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-10-23 10:05:05 +0700

    fix bug

--- src/lib/data/Comment.js
+++ src/lib/data/Comment.js
@@ -1,36 +1,5 @@
 // comment.js
-
-// GET /api/comments
-export function getComments() {
-  // Fetch comments from the server
-  // Example code - replace with your own implementation
-  return fetch('/api/comments')
-    .then(response => response.json())
-    .then(data => data.comments);
-}
-
-// POST /api/comments
-export function createComment(comment) {
-  // Send the new comment to the server
-  // Example code - replace with your own implementation
-  return fetch('/api/comments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(comment)
-  })
-    .then(response => response.json())
-    .then(data => data.comment);
-}
-
-// DELETE /api/comments/:id
-export function deleteComment(id) {
-  // Delete the comment from the server
-  // Example code - replace with your own implementation
-  return fetch(`/api/comments/${id}`, {
-    method: 'DELETE'
-  })
-    .then(response => response.json())
-    .then(data => data.deleted);
-}
+// Import the json helper function from @sveltejs/kit import { json } from ‘@sveltejs/kit’;
+// GET /api/comments export async function get(request) { // Fetch comments from the server // Example code - replace with your own implementation const response = await fetch(‘/api/comments’); const data = await response.json(); return json(data.comments); }
+// POST /api/comments export async function post(request) { // Send the new comment to the server // Example code - replace with your own implementation const comment = request.body; const response = await fetch(‘/api/comments’, { method: ‘POST’, headers: { ‘Content-Type’: ‘application/json’ }, body: JSON.stringify(comment) }); const data = await response.json(); return json(data.comment); }
+// DELETE /api/comments/:id export async function del(request) { // Delete the comment from the server // Example code - replace with your own implementation const { id } = request.params; const response = await fetch(/api/comments/${id}, { method: ‘DELETE’ }); const data = await response.json(); return json(data.deleted); }
