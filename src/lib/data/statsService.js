471fd1395e20016de90cfcb59bf4e71affe7a0be
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-10-27 12:05:24 +0700

    update code

--- src/lib/data/statsService.js
+++ src/lib/data/statsService.js
@@ -2,32 +2,15 @@ import database from '$lib/data/database';
 import analyticsService from '$lib/data/analyticsService';
 
 // Create an instance of the Database class
-const databaseInstance = database;
+const databaseInstance = new database();
 
 // Create an instance of the analyticsService class
 const analytics = new analyticsService(databaseInstance);
 
-// Define an async function to get the stats
 export async function getStats() {
-  // Use a try-catch block to handle errors
   try {
-    // Await the results of the async functions
-    const [visitsToday, totalVisits, totalVisitors, totalCountries] = await Promise.all([
-      analytics.getVisitsToday(),
-      databaseInstance.getTotalVisits(),
-      databaseInstance.getTotalVisitors(),
-      databaseInstance.getTotalCountries()
-    ]);
-
-    // Return an object with the stats
-    return {
-      visitsToday,
-      totalVisits,
-      totalVisitors,
-      totalCountries
-    };
+    return await analytics.getStats();
   } catch (error) {
-    // Log the error and rethrow it
     console.error('Error occurred while fetching stats:', error);
     throw error;
   }
