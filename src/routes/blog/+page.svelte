d7af98849a7624b9026ad2d9bf7823b88c8c5b48
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-01-06 07:26:23 +0700

    add code

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,9 +1,14 @@
 <script>
+  import ArrowLeftIcon from '$lib/components/ArrowLeftIcon.svelte'
+  import ArrowRightIcon from '$lib/components/ArrowRightIcon.svelte'
 	import PostsList from '$lib/components/PostsList.svelte'
 	import Pagination from '$lib/components/Pagination.svelte'
 	import { siteDescription } from '$lib/data/config'
+  /** @type {import('./$types').PageData} */
 
 	export let data
+  $: isFirstPage = data.page === 1
+  $: hasNextPage = data.posts[data.posts.length - 1]?.previous
 </script>
 
 
@@ -13,7 +18,35 @@
 </svelte:head>
 
 <h1>Blog</h1>
+<div class="flex flex-col flex-grow">
+  <header class="pt-4">
+    <h1 class="text-4xl font-bold tracking-tight sm:text-5xl">
+This is a page that shares my articles about my experiences in learning programming
+    </h1>
+    <p class="mt-6">All of my written content collected in one place</p>
+  </header>
 
+  <div class="mt-16 sm:mt-20">
 <PostsList posts={data.posts} />
+  </div>
 
 <Pagination currentPage={1} totalPosts={data.total} />
+
+  <div class="flex items-center justify-between pt-16 pb-8">
+    {#if !isFirstPage}
+      <a href={`/posts/${data.page - 1}`} data-sveltekit-prefetch>
+        <ArrowLeftIcon class="w-4 h-4" />
+        Previous
+      </a>
+    {:else}
+      <div />
+    {/if}
+
+    {#if hasNextPage}
+      <a href={`/posts/${data.page + 1}`} data-sveltekit-prefetch
+        >Next
+        <ArrowRightIcon class="w-4 h-4" />
+      </a>
+    {/if}
+  </div>
+</div>
