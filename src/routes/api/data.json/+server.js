c3dbd3fff5e417e1916a1456a630e270f3eaa74c
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-01-06 08:41:19 +0700

    update

--- src/routes/api/data.json/+server.js
+++ src/routes/api/data.json/+server.js
@@ -1,6 +1,23 @@
-{"id":1,
-"title_page": "Sample Title page",
-"description":"Sample description Page",
-"author":"admin",
-"content": "Sample Content  ",
+import database from '$lib/data/database';
+
+export async function load({ params }) {
+  // get data from database by id
+  const data = await database.get(params.id);
+
+  // check if data exists
+  if (data) {
+    // returns data as JSON
+    return {
+      status: 200,
+      body: data
+    };
+  } else {
+    // returns error 404 if data not found
+    return {
+      status: 404,
+      body: {
+        message: 'No data found'
+      }
+    };
+  }
 }
