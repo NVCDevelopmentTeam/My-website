b1b7af01e30030981db2b64fb03eacdae31a4b68
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-01-11 12:21:36 +0700

    update code

--- src/lib/components/accessibility.svelte
+++ src/lib/components/accessibility.svelte
@@ -1,9 +1,28 @@
 <script>
   let darkMode = false;
+	let fontSize = 10;
+  let contrast = false;
+  let colorFilters = false;
 
   function toggle() {
     darkMode = !darkMode;
     window.document.body.classList.toggle('dark');
+  }
+
+  function increaseFontSize() {
+    fontSize++;
+  }
+
+  function decreaseFontSize() {
+    fontSize--;
+  }
+
+  function toggleContrast() {
+    contrast = !contrast;
+  }
+
+  function toggleColorFilters() {
+    colorFilters = !colorFilters;
   }
 </script>
 
@@ -24,6 +43,49 @@
     Go light
   {:else}
     Go dark
+  {/if}
+</button>
+<button on:click={e => fontSize++}>Increase font size</button>
+<button on:click={e => fontSize--}>Decrease font size</button>
+<p style="font-size: {fontSize + 'px'}">Font size is: { fontSize }</p>
+
+<button class="contrast" type="button"
+  data-toggle="contrast"
+  data-target="contrast"
+  aria-controls="contrast"
+  aria-pressed={contrast}
+  aria-checked={contrast}
+  aria-label="Contrast"
+  on:click={toggleContrast}>
+  <svg viewBox="0 0 20 20" fill="none">
+    <path class="vert" d="M10 1V19" stroke="black" stroke-width="2" />
+    <path d="M1 10L19 10" stroke="black" stroke-width="2" />
+  </svg>
+  <span class="navbar-toggler-icon"></span>
+  {#if contrast }
+high 
+  {:else}
+low 
+   {/if}
+  </button>
+
+<button class="color-filters" type="button"
+  data-toggle="colorFilters"
+  data-target="colorFilters"
+  aria-controls="colorFilters"
+  aria-pressed={colorFilters}
+  aria-checked={colorFilters}
+  aria-label="Color filters"
+  on:click={toggleColorFilters}>
+  <svg viewBox="0 0 20 20" fill="none">
+    <path class="vert" d="M10 1V19" stroke="black" stroke-width="2" />
+    <path d="M1 10L19 10" stroke="black" stroke-width="2" />
+  </svg>
+  <span class="navbar-toggler-icon"></span>
+  {#if  colorFilters }
+default
+  {:else}
+chane 
   {/if}
 </button>
 
