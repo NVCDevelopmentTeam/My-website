a4e71d972c5b071493e15467273bbe6da70dc250
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-07-04 17:11:22 +0700

    update code

--- src/lib/data/config.js
+++ src/lib/data/config.js
@@ -3,27 +3,33 @@
  * in the <meta> tags, in the footer, and in the RSS feed.
  * 
  * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
- **/ 
+ **/
 
 export const siteTitle = 'Coding Nguyễn'
 export const siteDescription = 'Built with the SvelteKit Static Blog Starter'
 export const siteURL = 'example.com'
 export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
-export const siteAuthor = '- Coding nguyễn all rights reserved'
+export const siteAuthor = '- Coding Nguyễn all rights reserved'
 
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10
 
+// Social media links
+export const linkedin = 'your-linkedin-username'
+export const instagram = 'your-instagram-username'
+export const twitter = 'your-twitter-username'
+export const github = 'your-github-username'
+
 // Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
 export const navItems = [
-	{
-		title: 'Blog',
-		route: '/blog'
-	}, {
-		title: 'About',
-		route: '/about'
-	}, {
-		title: 'Contact',
-		route: '/contact' 
-	},
+  {
+    title: 'Blog',
+    route: '/blog'
+  }, {
+    title: 'About',
+    route: '/about'
+  }, {
+    title: 'Contact',
+    route: '/contact'
+  },
 ]
